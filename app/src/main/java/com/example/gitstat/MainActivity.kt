21387df2e54cbd2a3672847e8efe0ff6d91d4228
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-02-28 15:40:37 +0300

    Use navigation component

--- app/src/main/java/com/example/gitstat/MainActivity.kt
+++ app/src/main/java/com/example/gitstat/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.gitstat
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
