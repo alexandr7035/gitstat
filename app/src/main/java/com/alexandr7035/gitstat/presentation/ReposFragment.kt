e5b3d9abbcdfe1a9d117905ffb45744601892e98
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-08-22 13:11:44 +0300

    A little cleanup of the chart code

--- app/src/main/java/com/alexandr7035/gitstat/presentation/ReposFragment.kt
+++ app/src/main/java/com/alexandr7035/gitstat/presentation/ReposFragment.kt
@@ -1,20 +1,19 @@
-package com.alexandr7035.gitstat
+package com.alexandr7035.gitstat.presentation
 
 import android.content.Context
 import android.content.SharedPreferences
 import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import com.alexandr7035.gitstat.R
 import com.alexandr7035.gitstat.common.SyncStatus
 import com.alexandr7035.gitstat.databinding.FragmentReposBinding
-import com.alexandr7035.gitstat.presentation.MainViewModel
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
@@ -31,12 +30,10 @@ class ReposFragment : Fragment() {
     private val LOG_TAG = "DEBUG_TAG"
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var binding: FragmentReposBinding
-
     private lateinit var navController: NavController
+    private lateinit var viewModel: MainViewModel
 
     private lateinit var languagesList: TreeMap<String, Int>
-
-    private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -65,6 +62,10 @@ class ReposFragment : Fragment() {
         // NavController
         val hf: NavHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = hf.navController
+
+
+        // Setup chart configuration
+        setupLanguagesChart()
 
     }
 
@@ -95,17 +96,10 @@ class ReposFragment : Fragment() {
                 binding.privateReposCountView.text = privateReposCount.toString()
                 binding.publicReposCountView.text = publicReposCount.toString()
 
-                // Chart
-                binding.languagesChart.invalidate()
-
                 // Read colors jsom from resources
-                val inputStream = resources.openRawResource(R.raw.language_colors)
-                val reader = InputStreamReader(inputStream)
-                val builder = GsonBuilder()
-                val itemsMapType = object : TypeToken<Map<String, Map<String, String>>>() {}.type
-                val languagesColorsList: Map<String, Map<String, String>> = builder.create().fromJson(reader, itemsMapType)
+                val languagesColorsList: Map<String, Map<String, String>> = getLangColorsList()
 
-                // Diagram data
+                // Diagram data. Colors must correspond entries in their order in list
                 val entries = ArrayList<PieEntry>()
                 val diagramColors: MutableList<Int> = ArrayList()
 
@@ -139,6 +133,9 @@ class ReposFragment : Fragment() {
                 }
 
 
+                // Chart
+                binding.languagesChart.invalidate()
+
                 val dataSet = PieDataSet(entries, "")
                 dataSet.apply {
                     colors = diagramColors
@@ -150,29 +147,14 @@ class ReposFragment : Fragment() {
 
                 val pieData = PieData(dataSet)
                 // Remove decimal part from value
-                pieData.setValueFormatter(CustomValueFormatter())
+                pieData.setValueFormatter(object: ValueFormatter() {
+                    override fun getFormattedValue(value: Float): String {
+                        return "" + value.toInt()
+                    }
+                })
 
-
-                // Legend settings
-                val legend: Legend = binding.languagesChart.getLegend()
-                legend.apply {
-                    verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
-                    horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
-                    orientation = Legend.LegendOrientation.HORIZONTAL
-                    setDrawInside(false)
-                    isWordWrapEnabled = true
-                    textSize = 20f
-                    xEntrySpace = 20f
-                }
-
-
-                // General chart settings
+                // Update data
                 binding.languagesChart.apply {
-                    setEntryLabelTextSize(16f)
-                    setDrawSliceText(false)
-                    description.isEnabled = false
-                    setCenterTextSize(30f)
-
                     centerText = totalReposCount.toString()
                     // Should be in the end to display legend correctly
                     data = pieData
@@ -225,15 +207,43 @@ class ReposFragment : Fragment() {
         binding.languagesCard.visibility = View.GONE
     }
 
+
     private fun showRepositoriesViews() {
         binding.reposCountCard.visibility = View.VISIBLE
         binding.languagesCard.visibility = View.VISIBLE
     }
-}
 
 
-class CustomValueFormatter(): ValueFormatter() {
-    override fun getFormattedValue(value: Float): String {
-        return "" + value.toInt()
+    private fun setupLanguagesChart() {
+        // Legend settings
+        binding.languagesChart.legend.apply {
+            verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
+            horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
+            orientation = Legend.LegendOrientation.HORIZONTAL
+            setDrawInside(false)
+            isWordWrapEnabled = true
+            textSize = 20f
+            xEntrySpace = 20f
+        }
+
+        // General chart settings
+        binding.languagesChart.apply {
+            setEntryLabelTextSize(16f)
+            setDrawSliceText(false)
+            description.isEnabled = false
+            setCenterTextSize(30f)
+        }
+
+    }
+
+
+    private fun getLangColorsList(): Map<String, Map<String, String>> {
+        // Read colors jsom from resources
+        val inputStream = resources.openRawResource(R.raw.language_colors)
+        val reader = InputStreamReader(inputStream)
+        val builder = GsonBuilder()
+        val itemsMapType = object : TypeToken<Map<String, Map<String, String>>>() {}.type
+
+        return builder.create().fromJson(reader, itemsMapType)
     }
 }
