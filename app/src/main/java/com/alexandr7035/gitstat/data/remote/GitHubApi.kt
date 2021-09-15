4a007482ab15d6446befb3e06271eb8d538c7fce
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-09-16 01:29:15 +0300

    Remove outdated comment from GithubApi.kt

--- app/src/main/java/com/alexandr7035/gitstat/data/remote/GitHubApi.kt
+++ app/src/main/java/com/alexandr7035/gitstat/data/remote/GitHubApi.kt
@@ -8,16 +8,8 @@ import retrofit2.http.Header
 
 interface GitHubApi {
     @GET("/user")
-    //fun getUser(@Header("Authorization") auth: String, @Path("username") username: String): Call<UserModel>
     suspend fun getUser(@Header("Authorization") auth: String): Response<UserModel>
 
-    // Use search api because "/users/{username}/repos" doesn't return private repos
-    //
-    // NOTE: This query not returning forks
-    // Github support: When a fork has less stars than its parent,
-    // it is not indexed at all for code search.
-    // It won't show up even when you use fork:true.
-    // So fork:true is not used here deliberately
     @GET("/user/repos")
     suspend fun getRepositories(
         @Header("Authorization") auth: String): Response<List<RepositoryModel>>
