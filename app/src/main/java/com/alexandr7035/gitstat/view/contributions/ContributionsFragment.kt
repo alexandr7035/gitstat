6dd3885ff30aa4f0597a63128514f737dc825f6a
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-10-04 01:16:33 +0300

    Add header to contributions fragment
    Related #52

--- app/src/main/java/com/alexandr7035/gitstat/view/contributions/ContributionsFragment.kt
+++ app/src/main/java/com/alexandr7035/gitstat/view/contributions/ContributionsFragment.kt
@@ -1,11 +1,13 @@
 package com.alexandr7035.gitstat.view.contributions
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.alexandr7035.gitstat.R
 import com.alexandr7035.gitstat.data.local.model.ContributionsYear
 import com.alexandr7035.gitstat.databinding.FragmentContributionsBinding
 import com.github.mikephil.charting.formatter.ValueFormatter
@@ -13,6 +15,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.math.round
 
 @AndroidEntryPoint
 class ContributionsFragment : Fragment() {
@@ -51,6 +54,26 @@ class ContributionsFragment : Fragment() {
                 binding?.yearsViewPager?.setCurrentItem(years.size - 1, false)
 
             }
+        })
+
+
+        viewModel.getContributionsLiveData().observe(viewLifecycleOwner, { contributions ->
+
+            val totalContributions = contributions.sumOf { it.count }
+
+            Log.d("DEBUG_TAG", "total $totalContributions ${contributions.size}")
+
+            val contributionsRate = round((totalContributions.toFloat() / contributions.size.toFloat() * 100)) / 100F
+
+            Log.d("DEBUG_TAG", "rate ${totalContributions / contributions.size}")
+            Log.d("DEBUG_TAG", "rate $contributionsRate")
+
+            binding?.totalContributions?.text = totalContributions.toString()
+
+            binding?.contributionsRate?.text = getString(
+                R.string.contributions_rate,
+                contributionsRate
+            )
         })
         
     }
