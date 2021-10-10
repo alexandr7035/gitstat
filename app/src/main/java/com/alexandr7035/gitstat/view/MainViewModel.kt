e35bfe0b095261526d668c7ef3ebdb0a1265a73c
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-10-11 00:15:35 +0300

    Remove old methods from MainViewModel.kt
    Related #83

--- app/src/main/java/com/alexandr7035/gitstat/view/MainViewModel.kt
+++ app/src/main/java/com/alexandr7035/gitstat/view/MainViewModel.kt
@@ -1,9 +1,7 @@
 package com.alexandr7035.gitstat.view
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.alexandr7035.gitstat.core.DataSyncStatus
 import com.alexandr7035.gitstat.data.SyncRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -13,24 +11,12 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(private val syncRepository: SyncRepository): ViewModel() {
 
-    private val syncStatusLiveData = MutableLiveData<DataSyncStatus>()
-
     fun checkIfTokenSaved(): Boolean {
         return syncRepository.checkIfTokenSaved()
     }
 
     fun clearToken() {
         syncRepository.clearToken()
-    }
-
-    fun getSyncStatusLiveData(): MutableLiveData<DataSyncStatus> {
-        return syncStatusLiveData
-    }
-
-    fun syncData() {
-        viewModelScope.launch(Dispatchers.IO) {
-            syncRepository.syncAllData(syncStatusLiveData)
-        }
     }
 
     fun clearCache() {
