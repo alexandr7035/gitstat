2bc29e7fc8edc4a21a2679262f2b71cd7c9023d7
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-10-14 23:04:55 +0300

    Disable debug toast

--- app/src/main/java/com/alexandr7035/gitstat/view/login/LoginFragment.kt
+++ app/src/main/java/com/alexandr7035/gitstat/view/login/LoginFragment.kt
@@ -174,8 +174,6 @@ class LoginFragment: Fragment() {
     }
 
     private fun showTokenInstructions() {
-        // TODO
-        Toast.makeText(requireContext(), "TODO", Toast.LENGTH_SHORT).show()
         navController.navigate(R.id.action_loginFragment_to_webViewFragment)
     }
 
