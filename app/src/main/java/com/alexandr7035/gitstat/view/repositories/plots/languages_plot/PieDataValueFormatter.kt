dbcc3f49df8c89890b0b46315ac2f301843daaed
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-10-17 16:02:14 +0300

    Move languages plot code to a separate class
    Related #90

---
+++ app/src/main/java/com/alexandr7035/gitstat/view/repositories/plots/languages_plot/PieDataValueFormatter.kt
@@ -0,0 +1,10 @@
+package com.alexandr7035.gitstat.view.repositories.plots.languages_plot
+
+import com.github.mikephil.charting.formatter.ValueFormatter
+
+class PieDataValueFormatter: ValueFormatter() {
+    // Remove decimal part from value
+    override fun getFormattedValue(value: Float): String {
+        return "" + value.toInt()
+    }
+}
