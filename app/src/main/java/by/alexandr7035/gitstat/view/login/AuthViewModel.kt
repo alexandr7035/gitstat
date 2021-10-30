95136811474b7de730a31480a918fb592fea3dc9
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-10-31 01:53:17 +0300

    Remove unused AuthStatus.kt

--- app/src/main/java/by/alexandr7035/gitstat/view/login/AuthViewModel.kt
+++ app/src/main/java/by/alexandr7035/gitstat/view/login/AuthViewModel.kt
@@ -1,13 +1,8 @@
 package by.alexandr7035.gitstat.view.login
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import by.alexandr7035.gitstat.core.AuthStatus
 import by.alexandr7035.gitstat.data.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
