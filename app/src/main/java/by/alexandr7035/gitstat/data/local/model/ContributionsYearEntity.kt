cd79166d84a7979fc1bacc0d8cb33341ccbd705a
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-11-02 01:57:50 +0300

    Hot fix for serialization crash
    Related #131

--- app/src/main/java/by/alexandr7035/gitstat/data/local/model/ContributionsYearEntity.kt
+++ app/src/main/java/by/alexandr7035/gitstat/data/local/model/ContributionsYearEntity.kt
@@ -2,8 +2,9 @@ package by.alexandr7035.gitstat.data.local.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.io.Serializable
 
 @Entity(tableName = "contribution_years")
 data class ContributionsYearEntity(
     @PrimaryKey
-    var id: Int)
+    var id: Int): Serializable
