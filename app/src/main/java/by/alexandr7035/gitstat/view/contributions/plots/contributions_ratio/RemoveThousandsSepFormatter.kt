dd04cda5ec9cf74c07cc38585a848fd86715c4a5
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-11-04 22:39:15 +0300

    Fix max value on ratio chart
    Closes #143

---
+++ app/src/main/java/by/alexandr7035/gitstat/view/contributions/plots/contributions_ratio/RemoveThousandsSepFormatter.kt
@@ -0,0 +1,11 @@
+package by.alexandr7035.gitstat.view.contributions.plots.contributions_ratio
+
+import com.github.mikephil.charting.formatter.ValueFormatter
+
+// Simple solution to remove space like "1 000" in thousands
+// Do not use with Floats!de
+class RemoveThousandsSepFormatter : ValueFormatter() {
+    override fun getFormattedValue(value: Float): String {
+        return value.toInt().toString()
+    }
+}
