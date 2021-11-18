e87523957d3badc9e4d5337d991c773bdcd549a4
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-11-18 23:25:41 +0300

    Update FragmentContributionTypes.kt

--- app/src/main/java/by/alexandr7035/gitstat/view/contributions/plots/contributions_types/FragmentContributionTypes.kt
+++ app/src/main/java/by/alexandr7035/gitstat/view/contributions/plots/contributions_types/FragmentContributionTypes.kt
@@ -4,14 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import by.alexandr7035.gitstat.R
 import by.alexandr7035.gitstat.databinding.FragmentContributionTypesBinding
+import by.alexandr7035.gitstat.view.contributions.ContributionsViewModel
+import by.alexandr7035.gitstat.view.contributions.plots.contributions_types.model.ContributionTypesListToRecyclerItemsMapper
+import by.alexandr7035.gitstat.view.repositories.plots.languages_plot.PieDataValueFormatter
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import dagger.hilt.android.AndroidEntryPoint
 
 
+@AndroidEntryPoint
 class FragmentContributionTypes : Fragment() {
 
     private var binding: FragmentContributionTypesBinding? = null
+    private val viewModel by viewModels<ContributionsViewModel>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -26,6 +38,53 @@ class FragmentContributionTypes : Fragment() {
             findNavController().navigateUp()
         }
 
+        val adapter = TypesAdapter(showCount = true)
+        binding?.typesRecycler?.layoutManager = LinearLayoutManager(requireContext())
+        binding?.typesRecycler?.adapter = adapter
+
+        viewModel.getContributionTypesLiveData().observe(viewLifecycleOwner, { typesData ->
+            // Detect max value
+            // FIXME find better solution
+            val commits = typesData.sumOf { it.commitContributions }
+            val issues = typesData.sumOf { it.issueContributions }
+            val pullRequests = typesData.sumOf { it.pullRequestContributions }
+            val reviews = typesData.sumOf { it.pullRequestReviewContributions }
+            val repositories = typesData.sumOf { it.repositoryContributions }
+            val unknown = typesData.sumOf { it.unknownContributions }
+
+            val total = typesData.sumOf { it.totalContributions }
+
+            val entries = listOf<PieEntry>(
+                PieEntry(commits.toFloat(), "Commits"),
+                PieEntry(issues.toFloat(), "Issues"),
+                PieEntry(pullRequests.toFloat(), "Pull requests"),
+                PieEntry(reviews.toFloat(), "Code reviews"),
+                PieEntry(repositories.toFloat(), "Repositories"),
+                PieEntry(unknown.toFloat(), "Unknown")
+            )
+
+            val dataSet = PieDataSet(entries, "")
+
+            val colors = listOf<Int>(
+                ContextCompat.getColor(requireContext(), R.color.color_commits),
+                ContextCompat.getColor(requireContext(), R.color.color_issues),
+                ContextCompat.getColor(requireContext(), R.color.color_pull_requests),
+                ContextCompat.getColor(requireContext(), R.color.color_code_reviews),
+                ContextCompat.getColor(requireContext(), R.color.color_repositories),
+                ContextCompat.getColor(requireContext(), R.color.color_unknown_contributions),
+            )
+
+            dataSet.colors = colors
+            dataSet.setDrawValues(false)
+
+            binding?.typesChart?.setupPieChartView(isLegendEnabled = false)
+            binding?.typesChart?.setPieChartData(dataSet, PieDataValueFormatter())
+            binding?.typesChart?.centerText = total.toString()
+
+            binding?.typesChart?.invalidate()
+
+            adapter.setItems(ContributionTypesListToRecyclerItemsMapper.map(typesData, requireContext()))
+        })
     }
 
     override fun onDestroyView() {
--- app/src/main/java/by/alexandr7035/gitstat/view/contributions/plots/contributions_types/TypesAdapter.kt
+++ app/src/main/java/by/alexandr7035/gitstat/view/contributions/plots/contributions_types/TypesAdapter.kt
@@ -3,12 +3,13 @@ package by.alexandr7035.gitstat.view.contributions.plots.contributions_types
 import android.annotation.SuppressLint
 import android.graphics.drawable.GradientDrawable
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import by.alexandr7035.gitstat.databinding.ViewContributionTypeItemBinding
 import by.alexandr7035.gitstat.view.contributions.plots.contributions_types.model.TypesItem
 
-class TypesAdapter: RecyclerView.Adapter<TypesAdapter.ViewHolder>() {
+class TypesAdapter(private val showCount: Boolean = false): RecyclerView.Adapter<TypesAdapter.ViewHolder>() {
 
     private var items: List<TypesItem> = emptyList()
 
@@ -20,9 +21,15 @@ class TypesAdapter: RecyclerView.Adapter<TypesAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.countView.text = items[position].count.toString()
+
+        if (showCount) {
+            holder.binding.countView.text = items[position].count.toString()
+        }
+        else {
+            holder.binding.countView.visibility = View.GONE
+        }
         holder.binding.label.text = items[position].label
-//        holder.binding.percentageView.text = items[position].percentage
+        holder.binding.percentageView.text = items[position].percentage
 
         (holder.binding.coloredMark.background as GradientDrawable).setColor(items[position].color)
     }
