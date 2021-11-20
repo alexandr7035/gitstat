0db8514bcce873f35cc191aeadf8f434218dbc1d
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-11-20 18:05:44 +0300

    Add Long.getStringDateFromLong() extension

---
+++ app/src/main/java/by/alexandr7035/gitstat/extensions/Long.kt
@@ -0,0 +1,18 @@
+package by.alexandr7035.gitstat.extensions
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun Long.getStringDateFromLong(
+    stringFormat: String,
+    timezoneStr: String? = null,
+    locale: Locale = Locale.US
+): String {
+    val format = SimpleDateFormat(stringFormat, locale)
+
+    if (timezoneStr != null) {
+        format.timeZone = TimeZone.getTimeZone(timezoneStr)
+    }
+
+    return format.format(this)
+}
