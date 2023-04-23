f0098027fd6d39bab38f81c0be36d4709617d292
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-04-24 00:43:08 +0300

    Update fragment to use property delegate for viewbinding

--- app/src/main/java/by/alexandr7035/gitstat/view/repositories/overview/ReposOverviewFragment.kt
+++ app/src/main/java/by/alexandr7035/gitstat/view/repositories/overview/ReposOverviewFragment.kt
@@ -1,81 +1,65 @@
 package by.alexandr7035.gitstat.view.repositories.overview
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import by.alexandr7035.gitstat.R
 import by.alexandr7035.gitstat.core.extensions.navigateSafe
 import by.alexandr7035.gitstat.core.extensions.observeNullSafe
 import by.alexandr7035.gitstat.databinding.FragmentReposOverviewBinding
 import by.alexandr7035.gitstat.view.MainActivity
 import by.alexandr7035.gitstat.view.repositories.RepositoriesViewModel
 import by.alexandr7035.gitstat.view.repositories.plots.languages_plot.LanguagesPlot
+import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
 @AndroidEntryPoint
-class ReposOverviewFragment : Fragment() {
+class ReposOverviewFragment : Fragment(R.layout.fragment_repos_overview) {
 
-    private var binding: FragmentReposOverviewBinding? = null
+    private val binding by viewBinding(FragmentReposOverviewBinding::bind)
     private val viewModel by viewModels<RepositoriesViewModel>()
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        binding = FragmentReposOverviewBinding.inflate(inflater, container, false)
-        return binding?.root
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val plot = LanguagesPlot()
         // Setup chart configuration
-        if (binding?.languagesChart != null) {
-            plot.setupPlot(binding!!.languagesChart)
-        }
+        plot.setupPlot(binding.languagesChart)
 
-        viewModel.getRepositoriesLiveData().observeNullSafe(viewLifecycleOwner, { repos ->
+        viewModel.getRepositoriesLiveData().observeNullSafe(viewLifecycleOwner) { repos ->
 
             Timber.tag("DEBUG_TAG").d("repos $repos")
 
-            binding!!.totalReposCountView.text = repos.size.toString()
-            binding!!.privateReposCountView.text = repos.filter { it.isPrivate }.size.toString()
-            binding!!.publicReposCountView.text = repos.filter { !it.isPrivate }.size.toString()
+            binding.totalReposCountView.text = repos.size.toString()
+            binding.privateReposCountView.text = repos.filter { it.isPrivate }.size.toString()
+            binding.publicReposCountView.text = repos.filterNot { it.isPrivate }.size.toString()
 
-            // Show stup instead of plot if list is empty
-            if (repos.isNullOrEmpty()) {
-                binding?.languagesChart?.visibility = View.GONE
-                binding?.noReposStub?.visibility = View.VISIBLE
+            // Show stub instead of plot if list is empty
+            if (repos.isEmpty()) {
+                binding.languagesChart.visibility = View.GONE
+                binding.noReposStub.visibility = View.VISIBLE
             } else {
-                binding?.languagesChart?.visibility = View.VISIBLE
-                binding?.noReposStub?.visibility = View.GONE
+                binding.languagesChart.visibility = View.VISIBLE
+                binding.noReposStub.visibility = View.GONE
 
                 // Populate chart with data
                 plot.setLanguagesData(
-                    chart = binding!!.languagesChart,
-                    languages = viewModel.getLanguagesForReposList(repos),
-                    totalReposCount = repos.size
+                    chart = binding.languagesChart, languages = viewModel.getLanguagesForReposList(repos), totalReposCount = repos.size
                 )
             }
-        })
+        }
 
 
-        binding!!.toReposListBtn.setOnClickListener {
+        binding.toReposListBtn.setOnClickListener {
             findNavController().navigateSafe(ReposOverviewFragmentDirections.actionReposFragmentToRepositoriesListHostFragment())
         }
 
-        binding?.drawerBtn?.setOnClickListener {
+        binding.drawerBtn.setOnClickListener {
             (requireActivity() as MainActivity).openDrawerMenu()
         }
 
     }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        binding = null
-    }
-
 }
