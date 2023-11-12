a859823df3ed5189f9eb6473988bf6284d8d194b
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2023-11-12 21:09:16 +0300

    Remove unused click listener

--- app/src/main/java/by/alexandr7035/gitstat/view/contributions_grid/DayClickListener.kt
+++
@@ -1,8 +0,0 @@
-package by.alexandr7035.gitstat.view.contributions_grid
-
-import by.alexandr7035.gitstat.data.local.model.ContributionDayEntity
-
-// Pass clicks from cells -> months -> grid fragment using this interface
-interface DayClickListener {
-    fun onDayItemClick(contributionDay: ContributionDayEntity)
-}
