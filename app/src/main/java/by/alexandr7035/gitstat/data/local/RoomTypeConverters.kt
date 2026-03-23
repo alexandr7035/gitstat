075f291b0d3689daa6b21d5fc5a0d887a83467f2
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2026-03-23 21:42:37 +0100

    Migrate from kapt to KSP
    Closes #228

--- app/src/main/java/by/alexandr7035/gitstat/data/local/RoomTypeConverters.kt
+++ app/src/main/java/by/alexandr7035/gitstat/data/local/RoomTypeConverters.kt
@@ -6,19 +6,21 @@ import by.alexandr7035.gitstat.core.Language
 import by.alexandr7035.gitstat.data.local.model.RepoLanguage
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import kotlin.collections.arrayListOf
 
 @ProvidedTypeConverter
 class RoomTypeConverters(private val gson: Gson) {
 
     @TypeConverter
-    fun fromString(value: String?): ArrayList<String?>? {
-        val listType = object : TypeToken<ArrayList<String?>?>() {}.type
-        return gson.fromJson(value, listType)
+    fun fromString(value: String?): ArrayList<String> {
+        if (value.isNullOrBlank()) return arrayListOf()
+        val listType = object : TypeToken<ArrayList<String>>() {}.type
+        return gson.fromJson(value, listType) ?: arrayListOf()
     }
 
     @TypeConverter
-    fun fromArrayList(list: ArrayList<String?>?): String? {
-        return gson.toJson(list)
+    fun fromArrayList(list: ArrayList<String>?): String {
+        return gson.toJson(list ?: arrayListOf<String>())
     }
 
     @TypeConverter
